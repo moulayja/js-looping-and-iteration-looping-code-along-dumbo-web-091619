c2efe7e61c8616dc7537f65611e99cc89eabf9bc
Jamal Farah <moulayjam@gmail.com>
2019-10-26 03:44:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
 function countdown( countdown ) {
   while ( countdown > 0 ) {
+    console.log( countdown );
     countdown -= 1;
   }
   console.log( countdown );
